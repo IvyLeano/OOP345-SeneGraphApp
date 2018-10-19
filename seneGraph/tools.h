0063d1652d7392347dcc4d0f878a703c8fff48c6
unknown <IvyLeano@outlook.com>
2018-10-19 14:51:29 -0400

    seneGraph.cpp

---
+++ seneGraph/tools.h
@@ -0,0 +1,20 @@
+/* W01
+ * tools.h
+ * Ivy Leano
+ */
+
+#ifndef SICT_TOOLS_H 
+#define SICT_TOOLS_H
+
+namespace sict 
+{
+	// Performs a fool-proof integer entry
+
+	int getInt(int min, int max);
+
+	// Displays the user interface menu
+
+	int menu();
+}
+#endif
+
